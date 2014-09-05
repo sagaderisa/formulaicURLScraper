349a4b934972a09ebb4508b6c4e03c5f55b0085c
sagaderisa <bethanylquinn@gmail.com>
2014-09-05 11:49:28 -0400

    Update website_scraping_code.py

--- website_scraping_code.py
+++ website_scraping_code.py
@@ -4,19 +4,21 @@
 # This code will allow you to import a tab-delineated file, scrape specified sections from the websites, and then add the information
 # back into your file.
 
-# Functions to define: 1) open CSV, 2) formulate URLs from uniqueIDs in CSV, 3) search & scrape
-# and add to dictionary. 4) Write dictionary to file
 # I originally tried to use lxml, but it was a big pain in the neck so I used BeautifulSoup instead. 
 # A friend also recommended I try Scrapepy, because he says "it is a true web crawler instead of just a parser. I may try
 # that in the future.
 
-import bs4
-from bs4 import BeautifulSoup
-import requests
-import time
-import csv
+# Functions to define: 1) open CSV, 2) formulate URLs from uniqueIDs in CSV, 3) search & scrape
+# and add to dictionary. 4) Write dictionary to file
+
+
+import requests # Requests interacts with the web and pulls data from websites.
+import time # Time allows you to delay your requests so that you don't bombard the other person's server
+import csv # CSV is a module that helps with file handling
+import re # re stands for "regular expressions" and it allows you to recognize a pattern and pull information from it
 
 def createListFromTSV(tsvFile):
+	'''This function takes a tab-delineated file (tsvFile) and returns a list of dictionaries.'''
 	with open(tsvFile,"rU") as f:
 		fileReader = csv.DictReader(f,delimiter='\t')
 #		print type(fileReader)
@@ -27,6 +29,7 @@ def createListFromTSV(tsvFile):
 #		print fileList
 
 def createHeaderList(tsvFile):
+	'''This function creates a list of headers from a tab-delineated file (tsvfile).'''
 	with open(tsvFile,"rU") as myFile:
 		lines = myFile.read().split("\n")
 		if lines <2:
@@ -35,6 +38,7 @@ def createHeaderList(tsvFile):
 		return headers
 
 def writeDictToTSV(myList, destFile, tsvFile, newColumnName):
+	'''This function writes a list of dictionaries to a tsvFile.'''
 	headers = createHeaderList(tsvFile)
 	headers.append('URL')
 	headers.append(newColumnName)
@@ -46,39 +50,40 @@ def writeDictToTSV(myList, destFile, tsvFile, newColumnName):
 			destination.writerow(thing)
 
 def formulateURLList(fileList, uniqueIDColumnName, urlFormulaPrefix, urlFormulaSuffix=''):
+	'''This creates a list of URLs based on a list of unique identifiers.'''
 	for entry in fileList: # fileList is a list of dicts
 		uniqueID = entry.get(uniqueIDColumnName)
 		URL = urlFormulaPrefix+uniqueID+urlFormulaSuffix
 		entry['URL'] = URL
 	return fileList
-	
-	def scrape(URL, section):
+
+
+
+def scrape(URL, startQuote, endQuote):
 	'''string --> string
-	Scrapes data from a given section in a given URL. Sections must be 
-	written in html format, i.e. <div class="wrapper_std"> or <h3 class="currentVersion">
+	Scrapes data from a given section in a given URL. Currently, you need to write
+	startQuote and endQuote in regular expression-friendly format (i.e. put \ before 
+	some characters, \s instead of space, etc.).
 	'''
-	webpage = requests.get(URL, allow_redirects=False)
-	soup = BeautifulSoup(webpage.text)
-#	print soup
-	if "=" in section:
-		htmlSection = section[1:section.find("=")].split(" ")
-		htmlTagPrefix = htmlSection[0]
-		htmlTagSuffix = htmlSection[1]
-		htmlTag = section[section.find("=")+1:]
-		response = []
-		searchSection = '{0}, {1}_={2}'.format(htmlTagPrefix, htmlTagSuffix, htmlTag)
-#		print (searchSection)
-		for part in soup.find_all(searchSection):
-			response.append(part)
-	else:
-		response = []
-		for sect in soup.findAll(lambda tag:tag.name.lower()==section):
-			response.append(sect)
-	return response
+	webpage = requests.get(URL, allow_redirects=False).text
+#	print webpage
+	searchStr = str(startQuote + '(.+)' + endQuote)
+	scraping = re.search(searchStr,webpage)
+	if scraping == None:
+		print "Scraping program couldn't find the information for {0}.".format(URL)
+	return scraping.group(1)
+#	print scraping
 	time.sleep(1)
 
-
 def scrapeUpdateDict(**kwargs):
+	'''This is the core function of this module. If you are trying to scrape data from a 
+	series of websites with formulaic URLs based on a unique identifier (government sites are 
+	commonly formatted this way), put them in a tab-delineated file (you can frequently 
+	download some but not all of the information about a given set of government information). 
+	This function will read the tab-delineated file, pull out the unique identifier, formulate the URL, 
+	and scrape the data you specify from the website. (In order to do this, you'll need to know 
+	what the text or html is around the text you want to scrape.)
+	'''
 	#if you want to add a default, you can write variable = kwargs.get('variable',default) otherwise value is none
 	# steps are 1) create fileDict, 2) create urlDict, 3) scrape data, 
 	# 4) add data to newColumnName in fileDict, 5) write info/new info to csv_file or csv_file_destination
@@ -90,6 +95,8 @@ def scrapeUpdateDict(**kwargs):
 	urlFormulaSuffix = kwargs.get('urlFormulaSuffix','')
 	tsv_file_destination = kwargs.get('tsv_file_destination','tsv_file')
 	headers = kwargs.get('headers')
+	startQuote = kwargs.get('startQuote')
+	endQuote = kwargs.get('endQuote')
 # add other option to formulate URL Dict not from generic way (i.e. for bill summary URLs)	
 	kwargs['headers'].append('URL')
 	kwargs['headers'].append(newColumnName)
@@ -98,24 +105,18 @@ def scrapeUpdateDict(**kwargs):
 #	print fileList
 	for item in fileList:
 		print item.get('URL')
-		newInfo = scrape(item.get('URL'), htmlSection)
-#		print newInfo
+		newInfo = scrape(item.get('URL'), startQuote, endQuote)
+		print newInfo
 		item[newColumnName] = newInfo
 	writeDictToTSV(fileList, tsv_file_destination, tsv_file, newColumnName)
 		
-NTSBKwargsDict = { 'htmlSection' : 'p', 
+NTSBKwargsDict = { 'startQuote' : 'to\sprepare\sthis\saircraft\saccident\sreport\.', 'endQuote' : 'Index\sfor',
 'tsv_file' : '/Users/bethanylquinn/Desktop/Pyscripts/NTSB_Test.txt', 
 'uniqueIDColumnName' : 'Event Id', 'newColumnName' : 'Description',
 'urlFormulaPrefix' : 'http://www.ntsb.gov/aviationquery/brief.aspx?ev_id=',
 'urlFormulaSuffix' : '&key=1',
 'tsv_file_destination' : 'NTSBTestResult2.txt'}
 
-NTSBKwargsDict['headers']=createHeaderList('/Users/bethanylquinn/Desktop/Pyscripts/NTSB_Test.csv')
-
-webpage = requests.get('http://www.ntsb.gov/aviationquery/brief.aspx?ev_id=20140408X84430&key=1', allow_redirects=False)
-soup = BeautifulSoup(webpage.text)
-print(soup.prettify())
+NTSBKwargsDict['headers']=createHeaderList('/Users/bethanylquinn/Desktop/Pyscripts/NTSB_Test.txt')
 
 scrapeUpdateDict(**NTSBKwargsDict)
-
-
